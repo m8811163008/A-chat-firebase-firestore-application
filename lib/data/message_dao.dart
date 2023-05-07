c0ba69b9798ac2ff91862a7a9e90c9a00ddbc578
Milad Hesami <m8811163008@gmail.com>
2023-05-07 11:45:06 +0430

    sync data between devices

--- lib/data/message_dao.dart
+++ lib/data/message_dao.dart
@@ -3,15 +3,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'message.dart';
 
 /// Data access object
-/// 
-/// The data access object helps access whatever data you have 
+///
+/// The data access object helps access whatever data you have
 /// stored at the given Cloud Firestore reference.
 class MessageDao {
   // gets the root of the messages collection
   final CollectionReference collection =
       FirebaseFirestore.instance.collection('messages');
-  void saveMessage(Message message) {
-    collection.add(message.toJson());
+  void saveMessage(Message message) async {
+    final result = await collection.add(message.toJson());
+    print(result);
   }
 
   Stream<QuerySnapshot> getMessageStream() {
--- lib/ui/message_list.dart
+++ lib/ui/message_list.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../data/message.dart';
 import '../data/message_dao.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'message_widget.dart';
 
 class MessageList extends StatefulWidget {
   const MessageList({Key? key}) : super(key: key);
@@ -32,8 +34,7 @@ class MessageListState extends State<MessageList> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // TODO: Add Message DAO to _getMessageList
-            _getMessageList(),
+            _getMessageList(messageDao),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -69,7 +70,6 @@ class MessageListState extends State<MessageList> {
     );
   }
 
-  // TODO: Replace _sendMessage
   void _sendMessage(MessageDao messageDao) {
     if (_canSendMessage()) {
       final message = Message(
@@ -83,14 +83,40 @@ class MessageListState extends State<MessageList> {
     }
   }
 
-  // TODO: Replace _getMessageList
-  Widget _getMessageList() {
-    return const SizedBox.shrink();
+  Widget _getMessageList(MessageDao messageDao) {
+    return Expanded(
+        child: StreamBuilder<QuerySnapshot>(
+      stream: messageDao.getMessageStream(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: LinearProgressIndicator(),
+          );
+        }
+        return _buildList(context, snapshot.data!.docs);
+      },
+    ));
   }
 
-  // TODO: Add _buildList
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapShots) {
+    return ListView(
+      controller: _scrollController,
+      physics: const BouncingScrollPhysics(),
+      padding: EdgeInsets.only(top: 20),
+      children: snapShots
+          .map((snapshot) => _buildListItem(context, snapshot))
+          .toList(),
+    );
+  }
 
-  // TODO: Add _buildListItem
+  Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
+    final message = Message.fromSnapShot(snapshot);
+    return MessageWidget(
+      message.text,
+      message.date,
+      message.email,
+    );
+  }
 
   bool _canSendMessage() => _messageController.text.isNotEmpty;
 
